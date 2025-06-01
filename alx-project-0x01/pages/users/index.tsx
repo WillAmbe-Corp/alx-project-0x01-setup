2c8495aef21595b6e4f4653e9e35ecf3103d8afe
unknown <nebaprincewill237@gmail.com>
2025-06-01 16:53:20 +0100

    Implementation of user card

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,6 +1,8 @@
 import Header from "@/components/layout/Header";
 import UserCard from "@/components/common/UserCard";
-const Users: React.FC = ( {posts} ) => {
+import { UserProps } from "@/interfaces";
+
+const Users: React.FC<UserProps[]> = ( {posts} ) => {
   return (
     <div className="flex flex-col h-screen">
         <Header />
@@ -8,7 +10,7 @@ const Users: React.FC = ( {posts} ) => {
           <h1 className="text-2xl font-semibold">Add User</h1>
           <button className="bg-blue-700 px-4 py-2 rounded-full text-white">New User</button>
           <div className="flex flex-col gap-4">
-            {posts?.map(({name, username, id, phone, website, address, email, company}: UserProps, key: number)=>(
+            {posts.map(({name, username, id, phone, website, address, email, company}:UserProps, key: number)=>(
               <UserCard name={name} username={username} id={id} phone={phone} website={website} address={address} email={email} company={company} key={key} />
             ))}
           </div>
