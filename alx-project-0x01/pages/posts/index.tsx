4aefa39781a5a816125d373e9548ec03830732bf
unknown <nebaprincewill237@gmail.com>
2025-08-05 05:37:59 +0100

    aranging of error

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,7 +1,7 @@
 import PostCard from "@/components/common/PostCard";
 import PostModal from "@/components/common/PostModal";
 import Header from "@/components/layout/Header";
-import {  PostProps } from "@/interfaces";
+import { PostProps } from "@/interfaces";
 import { PostData } from "@/interfaces"
 import { useState } from "react";
 
